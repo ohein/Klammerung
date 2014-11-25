23094d16b620b9b5919776eb6d4190c72f33fed7
ohein <oliver.hein@t-online.de>
2014-11-25 21:19:19 +0100

    fgdfg

--- src/Quicksort.java
+++ src/Quicksort.java
@@ -1,6 +1,13 @@
 public class Quicksort {
 
+	public static void main(String[] args) {
+		int[] zahlen = {5, 8, 2, 3, 1, 9, 4, 7, 10, 6};
+		quicksort(zahlen, 0, 9);
+		ausgabe(zahlen, 0, 9);
+	}
+	
 	public static void quicksort(int[] a, int von, int bis) {
+		ausgabe(a, von, bis);
 		int v = a[(bis + von) / 2];
 		int i = von, j = bis;
 		do {
@@ -21,5 +28,11 @@ public class Quicksort {
 		int hlp=a[pos1];
 		a[pos1]=a[pos2];
 		a[pos2]=hlp;
+	}
+	
+	private static void ausgabe(int[] a, int b, int e){
+		for (int i=b; i<=e; i++)
+			System.out.print(a[i] + " ");
+		System.out.println();
 	}
 }
